e8c0b905703210131b4ee557dfa1c3020d7bfd21
gabriel <gabrielvegarosario@gmail.com>
2024-09-27 19:14:30 -0400

    se agrego la clase repositorio pedido y se implementaron sus operaciones crud

---
+++ Implementaciones/Repositorios/RepositorioPedido.cs
@@ -0,0 +1,50 @@
+﻿using Sistema_de_Inventario.Abstracciones.Repositorios;
+
+using Sistema_de_Inventario.DTOs.Pedido;
+using Sistema_de_Inventario.Models;
+
+namespace Sistema_de_Inventario.Implementaciones.Repositorios
+{
+    public class RepositorioPedido : IRepositorioPedido
+    {
+        private readonly InventorySystemContext _context;
+        public RepositorioPedido(InventorySystemContext context)
+        {
+            _context = context;
+        }
+
+        public Pedido Crear(CrearPedidoDTO crearPedidoDTO)
+        {
+            var pedido = new Pedido {
+            Total = crearPedidoDTO.Total,
+            FechaCreacion = DateTime.Now
+            };
+
+            var result = _context.Add(pedido);
+            _context.SaveChanges();
+
+            return result.Entity;
+        }
+
+        public void Borrar(int id)
+        {
+            var borrar = GetById(id);
+
+            if(borrar == null)
+            {
+                throw new Exception("Usuario no Encontrado");
+            }
+        }
+
+        public List<Pedido> Get()
+        {
+            return [.. _context.Pedidos];
+        }
+
+        public Pedido? GetById(int id) 
+        {
+            return _context.Pedidos.Where(p => p.PedidoId == id).FirstOrDefault();
+
+        }
+    }
+}
