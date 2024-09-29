b74b7b4fed9e7815515488a9c2c7d8dbc8f95d0f
gabriel <gabrielvegarosario@gmail.com>
2024-09-29 01:58:04 -0400

    se agrego un metodo para convertir de categoria a categoria dto

--- DTOs/Categoria/CategoriaDTO.cs
+++ DTOs/Categoria/CategoriaDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Sistema_de_Inventario.DTOs.Categoria
+﻿using Sistema_de_Inventario.Models;
+
+namespace Sistema_de_Inventario.DTOs.Categoria
 {
     public class CategoriaDTO
     {
@@ -6,5 +8,17 @@
         public string Nombre { get; set; } = null!;
         public string Descripcion { get; set; } = null!;
         public DateTime FechaCreacion { get; set; }
+
+        public static CategoriaDTO ConvertirACategoriaDTO(Sistema_de_Inventario.Models.Categoria categoria)
+        {
+            return new CategoriaDTO
+            {
+                CategoriaId = categoria.CategoriaId,
+                Nombre = categoria.Nombre,
+                Descripcion = categoria.Descripcion,
+                FechaCreacion = (DateTime)categoria.FechaCreacion
+            };
+        }
     }
+
 }
