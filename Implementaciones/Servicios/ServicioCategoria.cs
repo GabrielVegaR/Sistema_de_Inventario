f7242fb1c8bd16808799af3233a11d2ea4fd873a
gabriel <gabrielvegarosario@gmail.com>
2024-09-29 01:58:41 -0400

    se implementaron los servicios y sus operaciones crud

---
+++ Implementaciones/Servicios/ServicioCategoria.cs
@@ -0,0 +1,65 @@
+﻿using Sistema_de_Inventario.Abstracciones.Servicios;
+using Sistema_de_Inventario.Abstracciones.Repositorios;
+using Sistema_de_Inventario.DTOs.Categoria;
+using Sistema_de_Inventario.Models;
+
+namespace Sistema_de_Inventario.Implementaciones.Servicios
+{
+    public class ServicioCategoria : IServicioCategoria
+    {
+        private readonly IRepositorioCategoria repositorioCategoria;
+        public ServicioCategoria(IRepositorioCategoria repositorioCategoria)
+        {
+            this.repositorioCategoria = repositorioCategoria;
+        }
+
+        public CategoriaDTO Actualizar(int id, ActualizarCategoriaDTO actualizarCategoriaDTO)
+        {
+            var categoria = repositorioCategoria.Actualizar(id, actualizarCategoriaDTO);
+
+            var categoriaDTO = CategoriaDTO.ConvertirACategoriaDTO(categoria);
+
+            return categoriaDTO;
+        }
+
+        public CategoriaDTO? Crear(CrearCategoriaDTO crearCategoriaDTO)
+        {
+            var categoria = repositorioCategoria.Crear(crearCategoriaDTO);
+            if (categoria == null)
+            {
+                return null;
+            }
+            var categoriaDTO = CategoriaDTO.ConvertirACategoriaDTO(categoria);
+
+            return categoriaDTO;
+        }
+
+        public void Borrar(int id)
+        {
+            repositorioCategoria.Borrar(id);
+        }
+
+        public List<CategoriaDTO> Get()
+        {
+            var listaCategoria = repositorioCategoria.Get();
+            var categoriaDTO = new List<CategoriaDTO>();
+
+            foreach (Categoria categoria in listaCategoria)
+            {
+                var result = CategoriaDTO.ConvertirACategoriaDTO(categoria);
+                categoriaDTO.Add(result);
+            }
+
+            return categoriaDTO;
+        }
+
+        public CategoriaDTO? GetById(int id)
+        {
+            var categoria = repositorioCategoria.GetById(id);
+
+            var categoriaDTO = CategoriaDTO.ConvertirACategoriaDTO(categoria);
+
+            return categoriaDTO;
+        }
+    }
+}
